af54d38edccc0960cd165c45066b95b318399396
barbora_stavova <barbora.stavova@outlook.cz>
2024-09-23 08:27:51 +0200

    hello_world

---
+++ eshop_nevim/git_config.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+git config --global user.name "barbora_stavova"
+git config --global user.email "barbora.stavova@outlook.cz"
+echo "Přihlášení uživatele proběhlo v pořádku"
+#cd eshop_nevim
+#https://fonts.bunny.net
+#https://www.fontshare.com
