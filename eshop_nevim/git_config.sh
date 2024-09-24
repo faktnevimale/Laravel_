44695e0404a87959b31e719064ef8f254e529098
barbora_stavova <barbora.stavova@outlook.cz>
2024-09-24 07:45:28 +0200

    adding

--- eshop_nevim/git_config.sh
+++ eshop_nevim/git_config.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+composer create-project laravel/laravel hokuspokus
+git config --list
 git config --global user.name "barbora_stavova"
 git config --global user.email "barbora.stavova@outlook.cz"
 echo "Přihlášení uživatele proběhlo v pořádku"
+ls
+dir
+
 #cd eshop_nevim
+#php artisan serve
 #https://fonts.bunny.net
 #https://www.fontshare.com
