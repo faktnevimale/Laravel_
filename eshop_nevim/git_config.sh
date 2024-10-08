6da0996671c3c537fd0fa836ef5f7533b3d07e67
barbora_stavova <barbora.stavova@outlook.cz>
2024-10-08 07:47:05 +0200

    kamonevim

--- eshop_nevim/git_config.sh
+++ eshop_nevim/git_config.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+git config --list
+git config --global --unset-all user.name
+git config --global --unset-all user.email
 git config --global user.name "barbora_stavova"
 git config --global user.email "barbora.stavova@outlook.cz"
 echo "Přihlášení uživatele proběhlo v pořádku"
+ls
+dir
+
+
 #cd eshop_nevim
 #https://fonts.bunny.net
 #https://www.fontshare.com
