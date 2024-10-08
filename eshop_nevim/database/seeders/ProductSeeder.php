f32bd4dbcba6b80fad94962479e98d194dacb964
barbora_stavova <barbora.stavova@outlook.cz>
2024-10-08 08:26:53 +0200

    aaaa

--- eshop_nevim/database/seeders/ProductSeeder.php
+++ eshop_nevim/database/seeders/ProductSeeder.php
@@ -7,11 +7,17 @@ use Illuminate\Database\Seeder;
 
 class ProductSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+
     public function run(): void
     {
-        //
+        $faker = Faker::create();
+        for ($i=0; $i < 10; $i++) { 
+            DB::table('products')->insert([
+                'name' => $faker->word,
+                'description' => $faker->sentence,
+                'price' => $faker->randomFloat(2, 50, 1000),
+                'stock' => $faker->numberBetween(1, 100),
+            ]);
+        }
     }
 }
